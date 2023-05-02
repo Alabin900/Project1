1686881fdbbf6fd5d03632bcceb6e4ae59cdb481
ALABIN NATION <herbertolayinka02@gmail.com>
2023-05-02 02:20:00 -0700

    commit

---
+++ bootstrap-5.3.0-alpha1-dist/js/intro.js
@@ -0,0 +1,62 @@
+// var A = "peter";
+// var b = 5;
+// var c = true;
+// var d = null;
+// var object = {
+//     name: "peter",
+//     age: 20,
+//     isMale: true,
+// };
+// console.log(object);
+// var Array =
+
+
+
+
+
+
+
+// function Mutiplication(a, b) {
+
+//     console.log(a*b);
+// }
+// Mutiplication(20, 30);
+
+// let age =11;
+
+// function CheckGrade(){
+// if (grade.value == "")  {
+//     alert("please enter a valid age");
+// } else if (grade.value >= 70){
+//     alert("Excellent");   
+// }
+// if (grade.value == "")  {
+//     alert("please enter a valid age");
+// } else if (grade.value >= 70){
+//     alert("very good");   
+// }
+// if (grade.value == "")  {
+//     alert("please enter a valid age");
+// } else if (grade.value >= 70){
+//     alert("credit");   
+// }
+// if (grade.value == "")  {
+//     alert("please enter a valid age");
+// } else if (grade.value >= 70){
+//     alert("pass");   
+// }
+// if (grade.value == "")  {
+//     alert("please enter a valid age");
+// } else if (grade.value >= 70){
+//     alert("fail");   
+// }
+// }
+
+function CheckGrade() {
+    if(arguments. value == "") {
+        document. getElementsById("gon").innerHTML = "please enter a valid Grade";
+    } else if (age.value >= 18){
+        gon.innerHTML= "you are an adult";
+        gon.style.color= "green";
+    }
+}
---
+++ bootstrap-5.3.0-alpha1-dist/js/work.js
@@ -0,0 +1,9 @@
+function turnOn(){
+    document.getElementById("bulb").src="bulb-on.png";
+} 
+  
+
+  function turnOff() {
+    document.getElementById("bulb").src="bulb-off.png";
+  }
+  
